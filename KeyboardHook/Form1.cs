c68f0dd6a8b77206cf626374229c3695dfec2d2c
FlydisoftTech <flydisoft@gmail.com>
2021-02-05 22:13:24 +0900

    initial commit

---
+++ KeyboardHook/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Docs.v1;
+using Google.Apis.Docs.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+
+namespace KeyboardHook
+{
+    public partial class Form1 : Form
+    {
+        private IKeyboardMouseEvents m_GlobalHook;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            string[] Scopes = { DocsService.Scope.DocumentsReadonly };
+            string ApplicationName = "SmartSV";
+            UserCredential credential;
+
+/*            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }*/
+
+            // Create Google Docs API service.
+            var service = new DocsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = null,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String documentId = "1PT8NkFir3y74tbbPK1qNIjmw3xtzh39Cet8eaKpqyXU";
+            DocumentsResource.GetRequest request = service.Documents.Get(documentId);
+
+            // Prints the title of the requested doc:
+            // https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
+            Document doc = request.Execute();
+            Console.WriteLine("The title of the doc is: {0}", doc.Title);
+
+
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.KeyPress += GlobalHookKeyPress;
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine("KeyPress: \t{0}", e.KeyChar);
+        }
+    }
+}
